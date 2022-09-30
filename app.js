7addd73b37ddec7b29378a924da747ead75d2b01
Seth Silesky <silesky@users.noreply.github.com>
2022-09-29 21:59:07 -0500

    wip

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const express = require("express");
-
+import express from "express";
 const app = express();
-const PORT = 3000;
 
-app.listen(PORT, (error) => {
-  console.log("Error occurred, server can't start", error);
-});
+const server = app.listen(3000);
+app.get("/", (req, res) => res.send("Hello World!"));
+
+const onExit = () => {
+  setTimeout(async () => {
+    await analytics.closeAndFlush(); // flush all existing events
+    server.close(() => process.exit());
+  }, 0);
+};
+
+process.on("SIGINT", onExit);
+process.on("SIGTERM", onExit);
